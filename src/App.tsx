49771e6ce5d0d6a7e8bd92f3b8900bfa9a499ed1
ezhang07 <dangerwindow@gmail.com>
2025-10-04 15:58:44 -0700

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Maplibre from './component/maplibre'
 
@@ -9,34 +7,14 @@ function App() {
 
   return (
     <>
-<<<<<<< Updated upstream
       <header>
         <h1>Vancouver Washroom Finder</h1>
       </header>
       <main>
-        <div id="map" style={{ height: '100h', width: '100%' }}>
+        <div id="map" style={{ height: '100vh', width: '100%' }}>
           <Maplibre />
         </div>
       </main>
-=======
-      <div>
-        <button type="button">Click Me!</button>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Washroom Finder</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
-          count is {count}
-        </button>
-
-      </div>
-
->>>>>>> Stashed changes
     </>
   )
 }
